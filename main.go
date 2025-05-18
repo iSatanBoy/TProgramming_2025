343aec10a63ac93543bd1e2c4e3cdca521faa7ba
iSatanBoy <greentessera@gmail.com>
2025-05-18 22:09:10 +0300

    Выполнение 4 задания

--- main.go
+++ main.go
@@ -13,4 +13,10 @@ func main() {
 		y := math.Pow(arcsin*arcsin+math.Pow(arccos, 4), 3)
 		fmt.Println("При x =", v, "y =", y)
 	}
+	for i := 0.26; i <= 0.66; i += 0.08 {
+		arcsin := math.Asin(i)
+		arccos := math.Acos(i)
+		y := math.Pow(arcsin*arcsin+math.Pow(arccos, 4), 3)
+		fmt.Println("При x =", i, "y =", y)
+	}
 }
